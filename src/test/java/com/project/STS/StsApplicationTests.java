4b2cdb535c24bdc114a0096464b33aeb16d24e69
banapuramlakshmikanthreddy-creator <banapuramlakshmikanthreddy@gmail.com>
2026-03-30 10:48:58 +0530

    Initial commit - Spring Boot project

---
+++ src/test/java/com/project/STS/StsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.STS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
